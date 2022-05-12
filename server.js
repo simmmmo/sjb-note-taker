8c26babd86e33de7990db44c659cfd5949dcde36
Simon Browne <70622234+simmmmo@users.noreply.github.com>
2022-05-12 16:08:10 +1000

    Pushing to Update to Heroku

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // GET Route for homepage
-// app.get('/', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/index.html'))
-// );
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 // GET Route for Notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
